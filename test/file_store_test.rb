314ce1a543b49adbcdb1ca9f0c100083d5bcbc63
Daniel Pumford <daniel.pumford@zendesk.com>
2022-05-25 09:25:55 -0500

    Removes the copy_to_store! function

--- test/file_store_test.rb
+++ test/file_store_test.rb
@@ -134,29 +134,4 @@ describe RemoteFiles::FileStore do
       end
     end
   end
-
-  describe '#copy_to_store!' do
-    before do
-      @other_store = RemoteFiles::FileStore.new(:file_other)
-      @other_store[:directory] = Pathname.new(File.dirname(__FILE__)) + '../other_tmp'
-    end
-
-    describe 'when a file belongs to another store' do
-      before do
-        @file = RemoteFiles::File.new('other_id', :content_type => 'text/plain', :content => 'content')
-        @other_store.store! @file
-      end
-
-      it 'should show up in the new store' do
-        @other_store.copy_to_store!(@file, @store)
-        moved_file = @store.retrieve!(@file.identifier)
-        moved_file.identifier.must_equal @file.identifier
-        moved_file.stored_in.must_include @store.identifier
-      end
-
-      after do
-        @store.delete! @file.identifier
-      end
-    end
-  end
 end
